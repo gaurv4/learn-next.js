53393637aa8d61e79fd1c8fa00762c7153373a34
gaurv4 <github.gaurav04@gmail.com>
2024-03-12 06:36:33 -0700

    fixed csr api url

--- app/csr/users/[id]/page.tsx
+++ app/csr/users/[id]/page.tsx
@@ -8,7 +8,7 @@ const fetcher = async (url: string, ...args: any[]) => {
   };
 
 const page = ({ params }:any) => {
-    const { data, error, isLoading } = useSWR(`http://dummyjson.com/users/${params.id}`, fetcher)
+    const { data, error, isLoading } = useSWR(`https://dummyjson.com/users/${params.id}`, fetcher)
     if (error) {
         return (
             <h1 className="text-2xl m-4">Error happend! {error.message}</h1>
