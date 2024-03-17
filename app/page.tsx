e9896a1b72bc87e4ea39a7975a078e45c0620431
Gaurav Mahajan <142982401+gaurv4@users.noreply.github.com>
2024-03-17 02:11:21 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,17 @@ const page = () => {
     <>
       <div className="m-10 flex flex-col gap-4">
         <Link 
-         className="text-blue-500 hover:text-2xl hover:text-blue-800" 
-         href={'csr/users'}>ClientSideRendering</Link>
+          className="text-blue-500 hover:text-2xl hover:text-blue-800" 
+          href={'csr/users'}>ClientSideRendering
+        </Link>
         <Link 
-         className="text-blue-500 hover:text-2xl hover:text-blue-800" 
-         href={'ssr/users'}>ServerSideRendering</Link>
+          className="text-blue-500 hover:text-2xl hover:text-blue-800" 
+          href={'ssr/users'}>ServerSideRendering
+        </Link>
         <Link 
-         className="text-blue-500 hover:text-2xl hover:text-blue-800" 
-         href={'ssg/users'}>StaticSite</Link>
+          className="text-blue-500 hover:text-2xl hover:text-blue-800" 
+          href={'ssg/users'}>StaticSite
+        </Link>
       </div>
     </>
   )
